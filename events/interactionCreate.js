8657da24d6bbc922539412c6b2f161cd5d9c9698
Niridya <niridya@ndprotect.com>
2026-04-04 16:42:10 +0200

    Nouvelle syntaxe pour les ephemeral

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,5 @@
+const { MessageFlags } = require("discord.js");
+
 module.exports = {
     name: "interactionCreate",
     async execute(client, interaction) {
@@ -9,14 +11,14 @@ module.exports = {
             if (command.botOwnerOnly) {
                 if (!client.config.owners.includes(interaction.user.id)) return interaction.reply({
                     content: `❌ **Vous devez être le propriétaire du bot pour exécuter cette commande.**`,
-                    flags: ["Ephemeral"]
+                    flags: MessageFlags.Ephemeral
                 });
             };
 
             if (command.guildOwnerOnly) {
                 if (interaction.member.guild.ownerId != interaction.user.id && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
                     content: `❌ **Vous devez être le propriétaire du serveur pour exécuter cette commande.**`,
-                    flags: ["Ephemeral"]
+                    flags: MessageFlags.Ephemeral
                 });
             };
 
@@ -24,7 +26,7 @@ module.exports = {
                 const authorPerms = interaction.channel.permissionsFor(interaction.user) || interaction.member.permissions;
                 if (!authorPerms.has(command.permissions) && !client.config.owners.includes(interaction.user.id)) return interaction.reply({
                     content: `❌ **Vous n'avez pas les permissions nécessaires pour exécuter cette commande.**`,
-                    flags: ["Ephemeral"]
+                    flags: MessageFlags.Ephemeral
                 });
             };
 
